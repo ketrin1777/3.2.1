73ff8f464453297e8d6d8a7e783ff20a658eab84
nastya <nastenka29001@gmail.com>
2023-08-16 22:32:30 +0300

    Update

--- 3.2.1/3.2.1.cpp
+++ 3.2.1/3.2.1.cpp
@@ -3,18 +3,71 @@
 
 #include <iostream>
 
+enum Month
+{
+    None,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+};
+
+std::string montToString(Month& month)
+{
+    switch (month)
+    {
+    case None:
+        return "До свидания";
+    case January:
+        return "Январь";
+    case February:
+        return "Февраль";
+    case March:
+        return "Март";
+    case April:
+        return "Апрель";
+    case May:
+        return "Май";
+    case June:
+        return "Июнь";
+    case July:
+        return "Июль";
+    case August:
+        return "Август";
+    case September:
+        return "Сентябрь";
+    case October:
+        return "Октябрь";
+    case November:
+        return "Ноябрь";
+    case December:
+        return "Декабрь";
+    default:
+        break;
+    }
+    return "Неправильный номер!";
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    Month month = January;
+    while (month != None)
+    {
+        int montInt;
+        std::cout << "Введите номер месяца: ";
+        std::cin >> montInt;
+        month = static_cast<Month>(montInt);
+        std::cout << montToString(month) << std::endl;
+    }
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
